ab98b0bb902188ae43c8b1871356445db8001980
YosukeKono0101 <n11230380@qut.edu.au>
2024-04-16 01:39:58 +1000

    [MODIFY] BorrowMovie method
    member cannot borrow more than five movies and no more than one copy of the same title

--- CommunityLibraryDVDManagement/Member.cs
+++ CommunityLibraryDVDManagement/Member.cs
@@ -8,6 +8,7 @@ namespace CommunityLibraryDVDManagement
     public string ContactNumber { get; set; }
     public string Password { get; set; }
     private List<string> borrowedMovies;
+
     public Member(string firstName, string lastName, string contactNumber, string password)
     {
       FirstName = firstName;
@@ -17,12 +18,23 @@ namespace CommunityLibraryDVDManagement
       borrowedMovies = new List<string>();
     }
 
-    public void BorrowMovie(string movieTitle)
+    public bool BorrowMovie(string movieTitle)
     {
-      if(borrowedMovies.Count < 5 && !borrowedMovies.Contains(movieTitle))
+      if (borrowedMovies.Count >= 5)
       {
-        borrowedMovies.Add(movieTitle);
+        Console.WriteLine("Cannot borrow more than 5 DVDs at a time.");
+        return false;
       }
+
+      if (borrowedMovies.Contains(movieTitle))
+      {
+        Console.WriteLine("Cannot borrow more than one copy of the same movie.");
+        return false;
+      }
+
+      borrowedMovies.Add(movieTitle);
+      Console.WriteLine($"'{movieTitle}' has been successfully borrowed.");
+      return true;
     }
 
     public bool ReturnMovie(string movieTitle)
@@ -30,8 +42,10 @@ namespace CommunityLibraryDVDManagement
       if (borrowedMovies.Contains(movieTitle))
       {
         borrowedMovies.Remove(movieTitle);
+        Console.WriteLine($"'{movieTitle}' has been successfully returned.");
         return true;
       }
+      Console.WriteLine("This movie was not borrowed by you.");
       return false;
     }
 
